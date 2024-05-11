2122ed2eadc9a71810f2d9c0419d3249aae6a7ce
SkweesheeEmile <2359794@johnabbottcollege.net>
2024-05-11 14:31:38 -0400

    added the ai yahoo

--- test.py
+++ test.py
@@ -1,1 +1,24 @@
-print(" Yo MAMA")
+from openai import OpenAI
+
+client = OpenAI(
+        organization='org-zJMwRnES61tJScenlvoyZzXz',
+        project='proj_yBhKCBQ3jejnIRdrzos6d1Yr',
+        api_key='sk-proj-ZuczHEBzkxt0DUwDz1YTT3BlbkFJ8jgW8msTKW85MP2EM3wF'
+    )
+
+def main():
+   f = AIPrint("say yo mama")
+   print(f)
+
+def AIPrint(prompt): #print a message depending on the prompt that was passed to the function
+
+    stream = client.chat.completions.create(
+        model="gpt-3.5-turbo", #self explanatory, the model used
+        messages=[{"role": "user", "content": prompt}], #the prompt is in the content section
+        stream=True, 
+    )
+    for chunk in stream:
+        if chunk.choices[0].delta.content is not None:
+             hi = chunk.choices[0].delta.content, end=""
+             return hi
+main()
