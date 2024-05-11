6aa124a5a861fd0b124048dcce22058bda9d39bb
SkweesheeEmile <2359794@johnabbottcollege.net>
2024-05-11 15:53:24 -0400

    ADDED STUUF COS IM STUPID

--- Python-Scripts/Functionality.py
+++ Python-Scripts/Functionality.py
@@ -1,5 +1,22 @@
 def main():
+    Tutorial()    
+    gameover = False
+    while (gameover is False): #the actual game loop itself
+        for days in range(1,6): #the days loop, goes through day 1 to 5
+            print(days)
+            for messages in range(1,MessagesPerDay): #the messages the user gets per day dictated by the constant
+                prompt = input("Enter message here: ")
+                message = AIPrint(prompt)
+                    
+        gameover = True
+        #f = AIPrint("say yo mama")
+        #print(f)
+        
+        #print(gameover)
 
+
+def Tutorial():
+    print('Welcome to rizzler university, your goal? rizz up the ai. you have 5 days to get a date gl')
     nameCharacter = input("What is the name of your crush?")
 
 
